5db323db241960158a3c41021422cf31a267f4f5
NotHarshhaa <reddyharshhaa12@gmail.com>
2025-05-08 18:30:17 +0530

    jobs: fix: update currency format in formatMoney function to use INR
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,7 +11,7 @@ export function cn(...inputs: ClassValue[]) {
 export function formatMoney(amount: number) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: "USD",
+    currency: "INR",
   }).format(amount);
 }
 
